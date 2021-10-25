36619a4fa1c73bd3ffd34c76b58de14a61b43284
Matheus Brehm Barni <matheusbarni@cvccorp.com.br>
2021-10-25 10:05:57 -0300

    feat: added some libs and created routes

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <S.Wrapper>
+      Hello
+    </S.Wrapper>
+  );
+}
+
+export default Home;
---
+++ client/src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+    body {
+      font-size: 1.6rem;
+    }
+
+    --font: 'Roboto', sans-serif;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body, #__next {
+    height: 100%;
+  }
+  body {
+    font-family: var(--font), -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`
+
+export default GlobalStyles
